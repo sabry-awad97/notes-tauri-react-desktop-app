fa505b090c09aa2d866c4b8b7ef67628b0773ca2
Sabry Awad <dr.sabry1997@gmail.com>
2023-04-09 19:16:09 +0200

    Add comments and organize code in App.tsx
    Added comments to explain the purpose and functionality of each section of the code in App.tsx. Also added some extra white space to improve readability. No functional changes were made.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// Import necessary dependencies
 import { useState } from 'react';
 import Markdown from 'marked-react';
 
@@ -27,7 +28,9 @@ import { save, open, ask } from '@tauri-apps/api/dialog';
 import { writeTextFile, readTextFile } from '@tauri-apps/api/fs';
 import { sendNotification } from '@tauri-apps/api/notification';
 
+// Define the main App component
 function App() {
+  // Use the useLocalStorage hook to store and retrieve notes from local storage
   const [notes, setNotes] = useLocalStorage({
     key: 'my-notes',
     defaultValue: [
@@ -38,47 +41,60 @@ function App() {
     ],
   });
 
+  // Define state variables for the active note, its title and content, and the editor/preview toggle
   const [active, setActive] = useState(0);
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [checked, setChecked] = useState(false);
 
+  // Function to handle note selection
   const handleSelection = (title: string, content: string, index: number) => {
     setTitle(title);
     setContent(content);
     setActive(index);
   };
 
+  // Function to add a new note
   const addNote = () => {
     notes.splice(0, 0, { title: 'New note', content: '' });
     handleSelection('New note', '', 0);
     setNotes([...notes]);
   };
 
+  // Function to delete a note
   const deleteNote = async (index: number) => {
+    // Use the ask function from the Tauri API to confirm note deletion
     let deleteNote = await ask('Are you sure you want to delete this note?', {
       title: 'My Notes',
       type: 'warning',
     });
     if (deleteNote) {
+      // Remove the note from the notes array
       notes.splice(index, 1);
       if (active >= index) {
         setActive(active >= 1 ? active - 1 : 0);
       }
+      // If there are still notes remaining, set the content to the previous note's content
       if (notes.length >= 1) {
         setContent(notes[index - 1].content);
       } else {
+        // If there are no more notes, reset the title and content
         setTitle('');
         setContent('');
       }
+      // Update the notes array in local storage
       setNotes([...notes]);
     }
   };
 
+  // Render the main App component
   return (
     <div>
       <Grid grow m={10}>
+
+        {/* Left column */}
         <Grid.Col span="auto">
+          {/* Header section with app title and buttons */}
           <Flex gap="xl" justify="flex-start" align="center" wrap="wrap">
             <Flex>
               <ThemeIcon
@@ -86,7 +102,7 @@ function App() {
                 variant="gradient"
                 gradient={{ from: 'teal', to: 'lime', deg: 90 }}
               >
-                <IconNotebook size={32}/>
+                <IconNotebook size={32} />
               </ThemeIcon>
               <Text color="green" fz="xl" fw={500} ml={5}>
                 My Notes
@@ -107,6 +123,7 @@ function App() {
 
           <Divider my="sm" />
 
+          {/* Render the list of notes */}
           {notes.map((note, index) => (
             <Flex key={index}>
               <NavLink
@@ -123,7 +140,10 @@ function App() {
             </Flex>
           ))}
         </Grid.Col>
+
+        {/* Right column */}
         <Grid.Col span={2}>
+          {/* Editor/preview toggle */}
           <Switch
             label="Toggle Editor / Markdown Preview"
             checked={checked}
@@ -132,6 +152,7 @@ function App() {
 
           <Divider my="sm" />
 
+          {/* Render the editor or preview*/}
           {checked === false && (
             <div>
               <TextInput mb={5} />
@@ -146,6 +167,7 @@ function App() {
 
               <Divider my="sm" />
 
+              {/* Render the Markdown preview */}
               <Markdown>{content}</Markdown>
             </Paper>
           )}
@@ -155,4 +177,5 @@ function App() {
   );
 }
 
+// Export the App component as the default export
 export default App;
